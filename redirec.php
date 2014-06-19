cc6db8e4fb00a6c40eb8cbb28ffe6ce8def93ebd
soothion <soothion@sina.com>
2014-06-19 21:34:47 +0800

    Update redirec.php

--- redirec.php
+++ redirec.php
@@ -3,8 +3,9 @@ $v=$_REQUEST['v'];
 $v=  base64_decode($v);
 $path='uploadfile/video/';
 $file=$path.$v;
-$content = fread(fopen($file, 'r'), filesize($file));
-header("Content-type: video/*");
-echo $content;
+//$content = fread(fopen($file, 'r'), filesize($file));
+//header("Content-type: video/*");
+//echo $content;
+header("Location:$file");
 ?>
 
